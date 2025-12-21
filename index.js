bafeff55e92d8ace6e977c54938f903675caa1e1
rifat028 <istiakrifat001@gmail.com>
2025-12-21 19:13:20 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+var cors = require("cors");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@module54.p4tcocf.mongodb.net/?appName=Module54`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
+app.get("/", (req, res) => {
+  res.send("StyleDecor Is Running..!");
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run();
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`StyleDecor listening on port ${port}`);
 });
