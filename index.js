6db066bf56747e4f2a4a6d649442e6b96ba13807
rifat028 <istiakrifat001@gmail.com>
2025-12-23 00:13:16 +0600

    first deploy

--- index.js
+++ index.js
@@ -3,11 +3,39 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-var cors = require("cors");
+const cors = require("cors");
+const admin = require("firebase-admin");
 
-// middlewares
+// var serviceAccount = require("./styledecor-firebase-adminsdk.json");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
+  "utf8"
+);
+const serviceAccount = JSON.parse(decoded);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+// =========================== middlewares ==================
 app.use(cors());
 app.use(express.json());
+
+const verifyFbToken = async (req, res, next) => {
+  const token = req.headers.authorization;
+  console.log(token);
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized Access..!" });
+  }
+  try {
+    const idToken = token.split(" ")[1];
+    const decoded = await admin.auth().verifyIdToken(idToken);
+    console.log("decoded in the token", decoded);
+    req.decoded_email = decoded.email;
+    next();
+  } catch (err) {
+    return res.status(401).send({ message: "unauthorized access...!" });
+  }
+};
 
 const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@module54.p4tcocf.mongodb.net/?appName=Module54`;
 
@@ -27,11 +55,13 @@ app.get("/", (req, res) => {
 //APIs
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const database = client.db("StyleDecorDB");
     const serviceCollection = database.collection("services");
+    const bookingCollection = database.collection("bookings");
 
+    //================================ service APIs ==================================
     //create service
     app.post("/services", async (req, res) => {
       const newService = req.body;
@@ -48,7 +78,7 @@ async function run() {
 
     //Get all services
     app.get("/services", async (req, res) => {
-      console.log(req.query);
+      //   console.log(req.query);
 
       const { search_text, category, min_cost, max_cost } = req.query;
       let query = {};
@@ -58,7 +88,7 @@ async function run() {
         query = {
           serviceName: { $regex: search_text, $options: "i" },
         };
-        console.log("search Query is ", query);
+        // console.log("search Query is ", query);
       }
       if (req.query) {
         if (category && category != "all") query.serviceCategory = category;
@@ -67,10 +97,10 @@ async function run() {
         }
         if (min_cost) query.cost.$gte = Number(min_cost);
         if (max_cost) query.cost.$lte = Number(max_cost);
-        console.log("filter Query is ", query);
+        // console.log("filter Query is ", query);
         sort = { cost: 1 };
       }
-      console.log("final Query", query);
+      //   console.log("final Query", query);
       //   if()
       const cursor = serviceCollection.find(query).sort(sort);
       const result = await cursor.toArray();
@@ -84,6 +114,18 @@ async function run() {
         _id: new ObjectId(id),
       };
       const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
+
+    //================================ Booking APIs ==================================
+    // Create a service booking
+    app.post("/bookings", verifyFbToken, async (req, res) => {
+      const newBooking = req.body;
+      //   console.log("in Booking", req.headers);
+      if (newBooking.clientEmail !== req.decoded_email) {
+        return res.status(403).send({ message: "forbidden access...!" });
+      }
+      const result = await bookingCollection.insertOne(newBooking);
       res.send(result);
     });
 
